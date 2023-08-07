a3ed6b5b49a7a9cb2da10789d78ded827c3e351d
Aninda1999 <122980577+Aninda1999@users.noreply.github.com>
2023-08-07 12:33:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
-let low = 1;
-let high = 10; 
-  
-  let correct_ans = getRandomInt(low,high);
+function guess_game() {
+  let low = 1;
+  let high = 10;
+
+  let correct_ans = getRandomInt(low, high);
   console.log(correct_ans);
 
   function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  
-let c = 3;
-let input= [];
-for (let i = 0; i < 3; i++){
 
-input[i] = parseInt(prompt(`Guess a number between 1 to 10( ${c} chances left ):`));
+  let c = 3;
+  let input = [];
+  for (let i = 0; i < 3; i++) {
+    input[i] = parseInt(prompt(`Guess a number between 1 to 10 (${c} chances left):`));
 
-if(input[i]< correct_ans){
-  alert("Correct answer is higher!")
+    if (input[i] < correct_ans) {
+      alert("Correct answer is higher!");
+    } else if (input[i] > correct_ans) {
+      alert("Correct answer is lower!");
+    } else if (input[i] === correct_ans) {
+      alert("Yahoo! You Win!!");
+      displayRestartButton();
+      break;
+    } else {
+      alert("Please input something Valid!!");
+    }
+
+    c = c - 1;
+
+    if (c === 0) {
+      alert("Oh No! You Lose!!");
+      displayRestartButton();
+      break;
+    }
+  }
 }
-else if(input[i]> correct_ans){
-  alert("Correct answer is lower!")
+function displayRestartButton() {
+  let restartBtn = document.getElementById("restart-btn");
+  restartBtn.style.display = "block";
 }
 
-else if(input[i] === correct_ans){
-  alert("Yahoo! You Win!!")
-  break;
-}
-else{
-  alert("Please input something!!")
-}
+document.getElementById("restart-btn").addEventListener("click", guess_game)
 
-c = c-1;
 
-while(c === 0){
-  alert("Oh No! You Lose!!");
-  break;
-}
-}
+guess_game();
